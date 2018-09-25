de8ea31309d1f4322d62e622f1cf1895ab9b7a45
Maribmdj <maribmdj@gmail.com>
2018-09-25 01:33:16 -0600

    OTRAS TABLAS PRINCIPALES - PARTE I
    INCLUYEN LLAVES PRIMARIAS Y FORANEAS
    MARIELA

---
+++ Scripts/3 TABLA CORREO.sql
@@ -0,0 +1,32 @@
+/*
+Fecha: 24 SETIEMBRE 2018
+Autor: Mariela Barrantes
+Descripcion: TABLA CATALOGO CORREO
+*/
+create table CORREO
+(
+    ID_CORREO number(6),
+    NOMBRE varchar2(20) CONSTRAINT CORREO_NOMBRE_NN NOT NULL,    
+    ID_PERSONA NUMBER(6), CONSTRAINT FK_PERSONAC FOREIGN KEY (ID_PERSONA) REFERENCES PERSONA(ID_PERSONA),
+    --Atributos Auditoria
+    Fecha_creacion DATE,
+    Usuario_creacion VARCHAR2(10),
+    Fecha_ultima_modificacion DATE,
+    Usuario_ultima_modificacion VARCHAR2(10)
+)
+
+ALTER table CORREO add constraint pk_CORREO primary key (ID_CORREO)
+using index tablespace ce_ind pctfree 20 
+storage (initial 10k next 10k pctincrease 0);
+
+---------------------------------------------------------------
+--Comentarios de la tabla y atributos
+COMMENT ON TABLE CORREO IS 'CATALOGO CORREO';
+COMMENT ON COLUMN CE.CORREO.ID_CORREO IS 'IDENTIFICADOR ÚNICO DE LA TABLA';
+COMMENT ON COLUMN CE.CORREO.NOMBRE IS 'ATRIBUTO CON EL DETALLE DEL CORREO';
+COMMENT ON COLUMN CE.CORREO.ID_PERSONA IS 'ATRIBUTO REFERENCIADO DE LA TABLA PERSONA';
+
+COMMENT ON COLUMN CE.CORREO.Fecha_creacion IS 'ATRIBUTO AUDITORIA';
+COMMENT ON COLUMN CE.CORREO.Usuario_creacion IS 'ATRIBUTO AUDITOIA';
+COMMENT ON COLUMN CE.CORREO.Fecha_ultima_modificacion IS 'ATRIBUTO AUDITORIA';
+COMMENT ON COLUMN CE.CORREO.Usuario_ultima_modificacion IS 'ATRIBUTO AUDITORIA';
---
+++ Scripts/3 TABLA INSTITUCION.sql
@@ -0,0 +1,32 @@
+/*
+Fecha: 24 SETIEMBRE 2018
+Autor: Mariela Barrantes
+Descripcion: TABLA CATALOGO INSTITUCION
+*/
+
+create table INSTITUCION
+(
+    ID_INSTITUCION number(6),
+    NOMBRE varchar2(30) CONSTRAINT INSTITUCION_NOMBRE_NN NOT NULL,
+
+    --Atributos Auditoria
+    Fecha_creacion DATE,
+    Usuario_creacion VARCHAR2(10),
+    Fecha_ultima_modificacion DATE,
+    Usuario_ultima_modificacion VARCHAR2(10)
+)
+
+ALTER table INSTITUCION add constraint pk_INSTITUCION primary key (ID_INSTITUCION)
+using index tablespace ce_ind pctfree 20 
+storage (initial 10k next 10k pctincrease 0);
+
+---------------------------------------------------------------
+--Comentarios de la tabla y atributos
+COMMENT ON TABLE INSTITUCION IS 'CATALOGO INSTITUCION';
+COMMENT ON COLUMN CE.INSTITUCION.ID_INSTITUCION IS 'IDENTIFICADOR ÚNICO DE LA TABLA';
+COMMENT ON COLUMN CE.INSTITUCION.NOMBRE IS 'ATRIBUTO CON EL NOMBRE DE LA INSTITUCION';
+
+COMMENT ON COLUMN CE.INSTITUCION.Fecha_creacion IS 'ATRIBUTO AUDITORIA';
+COMMENT ON COLUMN CE.INSTITUCION.Usuario_creacion IS 'ATRIBUTO AUDITOIA';
+COMMENT ON COLUMN CE.INSTITUCION.Fecha_ultima_modificacion IS 'ATRIBUTO AUDITORIA';
+COMMENT ON COLUMN CE.INSTITUCION.Usuario_ultima_modificacion IS 'ATRIBUTO AUDITORIA';
