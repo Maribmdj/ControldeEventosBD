c33634f7167fee8f4ea3b19ce51ce560b9a85605
Maribmdj <maribmdj@gmail.com>
2018-10-01 23:29:58 -0600

    Insercion de datos a tablas catalogo
    Mariela

---
+++ Scripts/INSERTS/INSERTS TIPO PERSONA.sql
@@ -0,0 +1,17 @@
+/*
+Fecha: 01 OCTUBRE 2018
+Autor: Mariela Barrantes
+Descripcion: INSERT TIPOPERSONA
+*/
+
+INSERTS_CATALOGO.INSERTAR_CATALOGO_TIPOPERSONA('ADMINISTRADOR');
+INSERTS_CATALOGO.INSERTAR_CATALOGO_TIPOPERSONA('PARTICIPANTE');
+INSERTS_CATALOGO.INSERTAR_CATALOGO_TIPOPERSONA('PATROCINADOR');
+
+
+/*
+TIPOPERSONA: Activo - Inactivo - Cancelado - Pendiente
+tipoPersona: Administrador - Participante - Patrocinador
+tipoParticipante: Estudiante - Externo(Empresas)
+*/
+
