d2a5a951493d65f9d39c890c51ccfcc67af760be
Ignacio Jarquín <ignapoveda@gmail.com>
2018-10-05 22:06:08 -0500

    Estadistica de edades funcional

--- Edades.sql
+++ Edades.sql
@@ -4,39 +4,42 @@ Autor: Ignacio Jarquín Poveda
 Descripcion: Estadística de personas basadas en rangos de edad
 */
 
-Select (CASE when (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 18
-            then '0 - 18'
-            when (trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 18 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 30
-            then '19 - 30'
-            when(trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 30 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 45
-            then '31 - 45'
-            when (trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 45 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 55
-            then '46 - 55'
-            when(trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 55 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 65
-            then '56 - 65'
-            when (trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 65 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 75
-            then '66 - 75'
-            when (trunc(months_between(sysdate, fecha_nacimiento) / 12)) > 75 and (trunc(months_between(sysdate, fecha_nacimiento) / 12)) <= 85
-            then '76 - 85'
-            else '85+'
-            end)bucket,
-        count(*)
-    FROM(select (trunc(months_between(sysdate, fecha_nacimiento) / 12)) edad
-            FROM persona)
-    GROUP BY (case when edad <= 18
-                then '0 - 18'
-                when edad > 18 and edad <= 30
-                then '19 - 30'
-                when edad > 30 and edad <= 45
-                then '31 - 45'
-                when edad > 45 and edad <= 55
-                then '46 - 55'
-                when edad > 55 and edad <= 65
-                then '56 - 65'
-                when edad > 65 and edad <= 75
-                then '66 - 75'
-                when edad > 75 and edad <= 85
-                then '75 - 85' 
-                else '85+'
-            end);
-            
+Select '0 - 18' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc((sysdate-fecha_nacimiento)/365) between 0 and 18
+UNION
+select '19 - 30' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc((sysdate-fecha_nacimiento)/365) between 19 and 30
+UNION
+select '31 - 45' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc((sysdate-fecha_nacimiento)/365) between 31 and 45
+UNION
+select '46 - 55' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc((sysdate-fecha_nacimiento)/365) between 46 and 55
+UNION
+select '56 - 65' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc ((sysdate-fecha_nacimiento)/365) between 56 and 65
+UNION
+select '66 - 75' rango, count(1)
+from persona
+where fecha_nacimiento IS NOT NULL
+and trunc((sysdate-fecha_nacimiento)/365) between 66 and 75
+UNION
+select '76 - 85' rango, count(1)
+from persona
+where fecha_nacimiento is not null
+and trunc((sysdate-fecha_nacimiento)/365) between 75 and 85
+UNION
+select '85+' rango, count(1)
+from persona
+where fecha_nacimiento is not null
+and trunc((sysdate-fecha_nacimiento)/365) > 85;
